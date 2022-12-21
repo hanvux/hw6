3e0a319764fd3fbaf2986735f2925fd1c5df4838
hanvux <120507485+hanvux@users.noreply.github.com>
2022-12-21 10:12:27 +0700

    Update hw6.py

--- hw6.py
+++ hw6.py
@@ -1,30 +1,32 @@
 import csv
 
-with open('/content/sample_data/mnist_train_small.csv', 'r') as file:
-    reader = csv.reader(file)
-
 import one_gram_reader
 import matplotlib.pyplot as plt
 
-def n_counts(years, counts, total):
-    n_c = []
-    for i in range(len(years)):
-        n_c.append(counts[i]/total[years[i]])
-    return n_c
+import random as random
+
+def normalized_counts(years, counts, total):
+    years, counts = one_gram_reader.read_wfile(word, year_range, wfile)
+    total = one_gram_reader.read_total_counts(tfile)
+    nc = []
+    for i in range(len(counts)):
+        nc.append(counts[i]/total[years[i]])
+    return nc
     
-def p_words(words, year_range, wfile, tfile):
-    cls = ["b", "g", "r", "y", "p", "o"]
-    a = 0
-    for w in words:
-        n = w
-        years, counts = one_gram_reader.read_wfile(w, year_range, wfile)
+def plot_words(words, year_range, wfile, tfile):
+    r = random.random()
+    b = random.random()
+    g = random.random()
+    color = (r,b,g)
+    for word in words:
+        years, counts = one_gram_reader.read_wfile(word, year_range, wfile)
         total = one_gram_reader.read_total_counts(tfile)
-        n_c = n_c(years, counts, total)
-        cl = cls[a]
-        a += 1
-        plt.xlabel("Year")
-        plt.ylabel("Relative frequency")
-        plt.plot(years, n_c, cl, label = n)
-    plt.legend(framealpha=1, frameon=True)
-    plt.grid(cl='#828835', linestyle='dashed', linewidth=0.5)
+        nc = normalized_counts(years, counts, total)
+        
+    plt.figure()
+    plt.xlabel("Year")
+    plt.ylabel("Relative frequency")
+    plt.plot(years, nc, c = color)
+    plt.legend(word)
+    plt.grid(linestyle='dashed', linewidth=0.5)
     plt.show()
